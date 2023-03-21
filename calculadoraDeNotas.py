be23bbd8ef8d1614eb76f8ed05a403fe10e3da5c
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-03-21 10:46:59 -0300

    aula 2 modulo 4

---
+++ calculadoraDeNotas.py
@@ -0,0 +1,17 @@
+nome = str(input('insira um nome: '))
+notas = int(input('insira uma nota: '))
+students = {1: [nome], 2: [nome], 3: [nome]}
+nota = {1: [notas], 2: [notas], 3: [notas]}
+media = int(input('insira a media para  aprovação: '))
+
+def calculo(nota, media, students):
+    if nota == media or nota > media :
+        situacao = 'aprovado'
+    elif nota == 5 or nota < media :
+        situacao = "recuperação"
+    else:
+        situacao = "reprovado"
+    print(f'o {students} está: {situacao}')
+
+
+calculo(nota, media, students)
