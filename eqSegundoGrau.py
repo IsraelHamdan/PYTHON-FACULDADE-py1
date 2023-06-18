a0c1f662198fbf3ebbdbe474275ca28b2ac23ae8
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-06-17 21:55:13 -0300

    refatoring code

--- eqSegundoGrau.py
+++ eqSegundoGrau.py
@@ -1,39 +1,23 @@
 import cmath
 
-def vars():
-    a = int(input('insira o coeficiente a: '))
-    b = int(input('insira o coeficiente b: '))
-    c = int(input('insira o coeficiente c: '))
-    return a,b,c
+a = int(input('Insira o coeficiente a: '))
+b = int(input('Insira o coeficiente b: '))
+c = int(input('Insira o coeficiente c: '))
 
-a, b, c = vars()
-
-def delta(a, b, c):
-    delta = (b**2)-4*a*c
-    return delta
-
+delta = lambda a, b, c: (b**2) - 4*a*c
 d = delta(a, b, c)
 
 
-def raw(a, b, c, d):
+def raw(a,b, c, d):
+    raiz = ""
     if d > 0:
-        x1 = -b + cmath.sqrt(d) / (2*a)
-        x2 = -b - cmath.sqrt(d) / (2*a)
-        raw = f'a equação {a}x² + {b}x + {c} possui raizes x1 = {x1} e x2 = {x2}'
-        print(raw)
+        x1 = (-b + cmath.sqrt(d)/ (2*a))
+        x2 = (-b - cmath.sqrt(d)/ (2*a))
+        raiz = f'A equação {a}x² + {b}x + {c} possui raizes reais: x1 = {round(x1, 2)},x2 = {round(x2, 2)}'
     elif d == 0:
         x = -b / (2*a)
-        raw = f'a equação {a}x² + {b}x + {c} possui raiz x = + ou - {x}' 
-        print(raw)
+        raiz = f'A equação {a}x² + {b}x + {c} possui apenas uma raiz real: x = + ou - {round(x, 2)}'
     else:
-        raw = f'a equação {a}x² + {b}x + {c} NÃO possui raiz no conjunto dos reais pois o determinante da equação é: {d}, essa equação possui raizes no conjunto dos complexos'
-        print(raw)
-    # return x1, x2
-raw(a, b, c, d)
-
-
-
-
-
-
-
+        raiz = f'A equação {a}x² + {b}x + {c} NÃO possui raizes no conjunto dos reais, apenas nos complexos'
+    print(raiz)
+raw(a,b, c, d)
